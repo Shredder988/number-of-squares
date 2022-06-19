93bae8241f100e59c5f30e753bf24b8e22f2b1d6
Shredder988 <dimankolevatov@yandex.ru>
2022-06-19 22:44:13 +0300

    fourth commit

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shuldFindMax() {
+
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maxSalesMonth(sales);
+        long expected = 7;
+
+        assertEquals(expected, actual);
+    }
+}
+
